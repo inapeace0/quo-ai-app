f8e15ad8989a0af52637e9ae3914bf090ff8ee2d
brightness0302 <brightnes0302@gmail.com>
2024-04-09 10:55:30 +0200

    remove form builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
